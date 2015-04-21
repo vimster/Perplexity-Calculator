85db5523644a3289d1c99e3dafdb96a2f452dca2
vimster <mr.potsdam@gmx.de>
2015-04-21 11:00:03 +0200

    initial commit

---
+++ src/Main.hs
@@ -0,0 +1,21 @@
+module Main where
+
+-- import Network.HTTP
+
+jsonURL :: String
+jsonURL = "http://daniel-diaz.github.io/misc/pizza.json"
+
+-- 1. Perform a basic HTTP get request and return the body
+-- getContent :: String -> IO String
+-- getContent url = simpleHTTP (getRequest url) >>= getResponseBody
+
+addNumbers :: Integer -> Integer -> Integer
+addNumbers x y = x + y
+
+-- vorVier :: [Num] -> [Num]
+-- vorVier [] = [0]
+-- vorVier (x:xs) = filter (<4)
+
+main :: IO ()
+main = putStrLn "hello"
+
