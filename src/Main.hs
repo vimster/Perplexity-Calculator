edd3ae45f77619be02e02d565be0170df93f9ce3
vimster <mr.potsdam@gmx.de>
2015-04-22 00:34:07 +0200

    init frequency table

--- src/Main.hs
+++ src/Main.hs
@@ -4,32 +4,20 @@ import Control.Applicative
 import System.Directory(getCurrentDirectory, getDirectoryContents)
 import System.IO()
 import Text.XML.Light
+import qualified Data.Map as Map
 import Debug.Trace
 
 type Word = String
 type Sentence = [Word]
+type Frequencies = Map (Word, Word) Integer
 
 -- import Network.HTTP
 
 corpusPath :: FilePath
 corpusPath = "corpus/"
 
-jsonURL :: String
-jsonURL = "http://daniel-diaz.github.io/misc/pizza.json"
-
 isRegularFile :: FilePath -> Bool
 isRegularFile f = f /= "." && f /= ".."
-
--- 1. Perform a basic HTTP get request and return the body
--- getContent :: String -> IO String
--- getContent url = simpleHTTP (getRequest url) >>= getResponseBody
-
-addNumbers :: Integer -> Integer -> Integer
-addNumbers x y = x + y
-
--- vorVier :: [Num] -> [Num]
--- vorVier [] = [0]
--- vorVier (x:xs) = filter (<4)
 
 -- | read dir
 readDir :: String -> IO [FilePath]
@@ -47,15 +35,19 @@ parseXml source =
   in
     nestedWords
 
+createFrequencies :: [Sentence] -> Frequencies
+createFrequencies sentences = 
+
+lookupFrequency :: Frequencies -> Word -> Word -> Integer
+lookupFrequency frequencies w1 w2 = 
+
 calculatePerplexity :: Sentence -> Double
 calculatePerplexity sentence = 0.9
 
 main :: IO ()
 main = do
   files <- readDir corpusPath
-  print files
   let filePaths = map (corpusPath++) files
-  print filePaths
   contents <- mapM readFile filePaths
   let sentences = map parseXml contents
   print sentences
